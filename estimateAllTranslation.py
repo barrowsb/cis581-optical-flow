ce1fb6451cbee833945a926cca5fda89542bd6be
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-14 00:25:19 +0000

    Upload New File

---
+++ estimateAllTranslation.py
@@ -0,0 +1,14 @@
+
+# (INPUT) startXs: NxF matrix representing the starting X coordinates of all the features in the first frame for all the bounding boxes
+# (INPUT) startYs: NxF matrix representing the starting Y coordinates of all the features in the first frame for all the bounding boxes
+# (INPUT) img1: HxWx3 matrix representing the first image frame
+# (INPUT) img2: HxWx3 matrix representing the second image frame
+# (OUTPUT) newXs: NxF matrix representing the new X coordinates of all the features in all the bounding boxes
+# (OUTPUT) newYs: NxF matrix representing the new Y coordinates of all the features in all the bounding boxes
+
+
+def estimateAllTranslation(startXs,startYs,img1,img2):
+    
+    
+    
+    return newXs,newYs
