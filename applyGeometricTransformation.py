ee2344243b1a32162d06712b87f9b9051766c0c1
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-15 00:56:06 +0000

    Replace applyGeometricTransformation.py

--- applyGeometricTransformation.py
+++ applyGeometricTransformation.py
@@ -1,8 +1,9 @@
+
 # (INPUT) startXs: NxF matrix representing the starting X coordinates of all the features in the first frame for all the bounding boxes
 # (INPUT) startYs: NxF matrix representing the starting Y coordinates of all the features in the first frame for all the bounding boxes
 # (INPUT) newXs: NxF matrix representing the second X coordinates of all the features in the first frame for all the bounding boxes
 # (INPUT) newYs: NxF matrix representing the second Y coordinates of all the features in the first frame for all the bounding boxes
-# (INPUT) bbox: Fx4x2 matrix representing the four new corners of the bounding box where F is the number of detected objects
+# (INPUT) bbox: 4x2xF matrix representing the four new corners of the bounding box where F is the number of detected objects
 # (OUTPUT) newXs: N1xF matrix representing the X coordinates of the remaining features in all the bounding boxes after eliminating outliers
 # (OUTPUT) newYs: N1xF matrix representing the Y coordinates of the remaining features in all the bounding boxes after eliminating outliers
 # (OUTPUT) newbbox: Fx4x2 the bounding box position after geometric transformation
@@ -19,6 +20,8 @@ def applyGeometricTransformation(startXs,startYs,newXs,newYs,bbox):
     i = 0
     sum_shift_x = 0
     sum_shift_y = 0
+    
+
     while(i < len(startXs)):
     
         # If the change in feature position exceeds 4 pixels in x or y
