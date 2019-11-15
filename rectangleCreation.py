7159ea5cdf1812b0d2f96d32b33fcda5c264a97e
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-15 00:54:27 +0000

    Replace rectangleCreation.py

--- rectangleCreation.py
+++ rectangleCreation.py
@@ -1,13 +1,19 @@
 
 # (INPUT) frame: HxWx3 matrix representing the RGB image 
 # (OUTPUT) bbox: 4x2xF matrix representing four corners of rectangle, where F is each rectangle
-# (OUTPUT) bboxImg: HxWx3 matrix representing RGB with overlayed rectangle
 
 import cv2
 import numpy as np
 
 def rectangleCreation(frame):
-    x,y,w,h = 145,160,55,80  # Rectangle position and dimension
-    bbox = np.array([[x,y],[x+w,y],[x,y+h],[x+w,y+h]])
+    n_boxes = 2  # Number of bounding boxes manually creating
+    
+    x1,y1,w1,h1 = 145,160,55,80  # First Rectangle position and dimension
+    x2,y2,w2,h2 = 250,100,100,100  # Second Rectangle
+    
+    # Construct Bounding Box
+    bbox = np.zeros((4,2,n_boxes))
+    bbox[:,:,0] = np.array([[x1,y1],[x1+w1,y1],[x1,y1+h1],[x1+w1,y1+h1]])
+    bbox[:,:,1] = np.array([[x2,y2],[x2+w2,y2],[x2,y2+h2],[x2+w2,y2+h2]])
     
     return bbox
