34b0b080ba136c741d4163846c1f8eacc995d32a
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-20 23:24:11 +0000

    Update estimateFeatureTranslation.py

--- estimateFeatureTranslation.py
+++ estimateFeatureTranslation.py
@@ -1,4 +1,3 @@
-
 # (INPUT) startX: Represents the starting X coordinate for a single feature in the first frame
 # (INPUT) startY: Represents the starting Y coordinate for a single feature in the first frame
 # (INPUT) Ix: HxW matrix representing the gradient along the X-direction
@@ -7,6 +6,9 @@
 # (INPUT) img2: HxWx3 matrix representing the second image frame
 # (OUTPUT) newX: Represents the new X coordinate for a single feature in the second frame
 # (OUTPUT) newY: Represents the new Y coordinate for a single feature in the second frame
+
+import numpy as np
+import math
 
 def estimateFeatureTranslation(startX,startY,Ix,Iy,img1,img2):
     
