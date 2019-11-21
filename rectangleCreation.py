9e6c295996b0d54211dd740f23f8e40cca106bad
Michael Woc <mwoc@seas.upenn.edu>
2019-11-21 01:16:15 +0000

    Replace rectangleCreation.py

--- rectangleCreation.py
+++ rectangleCreation.py
@@ -5,15 +5,21 @@
 import cv2
 import numpy as np
 
-def rectangleCreation(frame):
-    n_boxes = 2  # Number of bounding boxes manually creating
-    
-    x1,y1,w1,h1 = 145,160,55,80  # First Rectangle position and dimension
-    x2,y2,w2,h2 = 250,100,100,100  # Second Rectangle
+def rectangleCreation(frame,n_boxes):
+    #n_boxes = 2  # Number of bounding boxes manually creating
+    bbox = np.zeros((4,2,n_boxes))
+    for i in range(n_boxes):
+        (x, y, bw, bh) = cv2.selectROI(frame)
+        cv2.destroyAllWindows()
+        bbox[:,:,i] = np.array([[x,y],[x+bw,y],[x,y+bh],[x+bw,y+bh]])
+
+    print(bbox)        
+#    x1,y1,w1,h1 = 145,160,55,80  # First Rectangle position and dimension
+#    x2,y2,w2,h2 = 250,100,100,100  # Second Rectangle
     
     # Construct Bounding Box
-    bbox = np.zeros((4,2,n_boxes))
-    bbox[:,:,0] = np.array([[x1,y1],[x1+w1,y1],[x1,y1+h1],[x1+w1,y1+h1]])
-    bbox[:,:,1] = np.array([[x2,y2],[x2+w2,y2],[x2,y2+h2],[x2+w2,y2+h2]])
+#    bbox = np.zeros((4,2,n_boxes))
+#    bbox[:,:,0] = np.array([[x1,y1],[x1+w1,y1],[x1,y1+h1],[x1+w1,y1+h1]])
+#    bbox[:,:,1] = np.array([[x2,y2],[x2+w2,y2],[x2,y2+h2],[x2+w2,y2+h2]])
     
     return bbox
