49b7f21d6682ad42d4082f282683d358ebcb1cc0
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-21 02:43:27 +0000

    Replace estimateFeatureTranslation.py

--- estimateFeatureTranslation.py
+++ estimateFeatureTranslation.py
@@ -1,40 +1,67 @@
 
-# (INPUT) startXs: NxF matrix representing the starting X coordinates of all the features in the first frame for all the bounding boxes
-# (INPUT) startYs: NxF matrix representing the starting Y coordinates of all the features in the first frame for all the bounding boxes
+# (INPUT) startX: Represents the starting X coordinate for a single feature in the first frame
+# (INPUT) startY: Represents the starting Y coordinate for a single feature in the first frame
+# (INPUT) Ix: HxW matrix representing the gradient along the X-direction
+# (INPUT) Iy: HxW matrix representing the gradient along the Y-direction
 # (INPUT) img1: HxWx3 matrix representing the first image frame
 # (INPUT) img2: HxWx3 matrix representing the second image frame
-# (OUTPUT) newXs: NxF matrix representing the new X coordinates of all the features in all the bounding boxes
-# (OUTPUT) newYs: NxF matrix representing the new Y coordinates of all the features in all the bounding boxes
+# (OUTPUT) newX: Represents the new X coordinate for a single feature in the second frame
+# (OUTPUT) newY: Represents the new Y coordinate for a single feature in the second frame
 
 import numpy as np
-from scipy.interpolate import interp2d
 import math
-import cv2
-from estimateFeatureTranslation import *
 
-
-def estimateAllTranslation(startXs,startYs,img1,img2): 
+def estimateFeatureTranslation(startX,startY,Ix,Iy,img1,img2):
     
-    # Convert Images to Grayscale
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    # Sampling Window Side Length
+    size_window = 9
+    
+    # Image Dimensions
+    xMax, yMax = img1.shape
+
+    # Temporal Gradient
+    It = img2 - img1
+    
+    # Use these???
+    #x_grid, y_grid = np.meshgrid(n_cols, n_rows)
+    #f = interp2d(x_grid, y_grid)
+    
+    # Center of Window is an individual feature coordinate
+    x_center = startX
+    y_center = startY
+    
+    sum_Ix_Ix = 0 
+    sum_Ix_Iy = 0
+    sum_Iy_Iy = 0
+    sum_Ix_It = 0
+    sum_Iy_It = 0
+    TL_x = int(x_center - math.floor(size_window/2))
+    TL_y = int(y_center - math.floor(size_window/2))
         
-    # Sobel Gradient Filters
-    Ix = cv2.Sobel(img1,cv2.CV_64F,1,0,ksize=5)
-    Iy = cv2.Sobel(img1,cv2.CV_64F,0,1,ksize=5)
-    
-    # Dimensional Parameters
-    n_rows,n_cols = img1.shape
-    n_features = startXs.shape[0] 
+    # Loop through window around feature
+    for r in range(size_window):
+        for c in range(size_window):
+            if ((TL_x+r < xMax) & (TL_y+c < yMax) & (TL_x-r >= 0) & (TL_y-c >= 0)):
+                sum_Ix_Ix = sum_Ix_Ix + Ix[TL_x+r,TL_y+c] * Ix[TL_x+r,TL_y+c]
+                sum_Ix_Iy = sum_Ix_Iy + Ix[TL_x+r,TL_y+c] * Iy[TL_x+r,TL_y+c]
+                sum_Iy_Iy = sum_Iy_Iy + Iy[TL_x+r,TL_y+c] * Iy[TL_x+r,TL_y+c]
+                sum_Ix_It = sum_Ix_It + Ix[TL_x+r,TL_y+c] * It[TL_x+r,TL_y+c]
+                sum_Iy_It = sum_Iy_It + Iy[TL_x+r,TL_y+c] * It[TL_x+r,TL_y+c]
 
-    # Feature Translation
-    newXs = np.zeros((n_features,1))
-    newYs = np.zeros((n_features,1))
-    for i in range(n_features):
-        startX = startXs[i]
-        startY = startYs[i]
-        newX,newY = estimateFeatureTranslation(startX,startY,Ix,Iy,img1,img2)
-        newXs[i] = newX
-        newYs[i] = newY    
+    # A Matrix in KLT Linear System Equation
+    gradientMatrix = np.array([[sum_Ix_Ix, sum_Ix_Iy],[sum_Ix_Iy, sum_Iy_Iy]])
+            
+    # b Matrix in KLT Linear System Equation
+    temporalMatrix = -np.array([[sum_Ix_It],[sum_Iy_It]])
+        
+    # Solving Linear System Equation    
+    A = np.squeeze(np.transpose(gradientMatrix))
+    b = np.squeeze(temporalMatrix)
+    flow = np.dot(A,b)
     
-    return newXs,newYs
+    newX = startX + int(flow[0])
+    newY = startY + int(flow[1])
+    
+    #print(flow)
+    
+    return newX,newY
