d23df8a2602ce5b2baa2a6969eaf66788627b5de
barrowsb <barrows.b7@gmail.com>
2019-11-21 00:41:42 +0000

    Replace wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -8,9 +8,26 @@ Date: Nov 13, 2019
 from objectTracking import *
 import cv2
 
+# Pick Video
+while True:
+    choice = input("Choose difficulty... Type 'e' for easy, 'm' for medium, 'h' for hard: ").lower()
+    if choice == 'e':
+        vid = 'Easy.MP4'
+        n_boxes = 2
+        break
+    elif choice == 'm':
+        vid = 'Medium.MP4'
+        n_boxes = 2
+        break
+    elif choice == 'h':
+        vid = 'hard.MP4'
+        n_boxes = 1
+        break
+    else:
+        print('INVALID INPUT. Try again.')
+
 # Import Raw Video
-rawVideo = cv2.VideoCapture('Easy.MP4')
+rawVideo = cv2.VideoCapture(vid)
 
 # Create Tracking Video
-trackedVideo = objectTracking(rawVideo)
-
+trackedVideo = objectTracking(rawVideo,n_boxes)
