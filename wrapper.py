ba1d7b2cbe74767a61245209b0a2f1a0042633c4
barrowsb <barrows.b7@gmail.com>
2019-11-23 23:22:09 +0000

    Replace wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -5,7 +5,7 @@ Authors: Barrows, Fisher, Woc
 Date: Nov 13, 2019
 '''
 
-from objectTracking import *
+from objectTracking import objectTracking
 import cv2
 
 # Pick Video
@@ -13,15 +13,21 @@ while True:
     choice = input("Choose difficulty... Type 'e' for easy, 'm' for medium, 'h' for hard: ").lower()
     if choice == 'e':
         vid = 'Easy.MP4'
-        n_boxes = 2
         break
     elif choice == 'm':
         vid = 'Medium.MP4'
-        n_boxes = 1
         break
     elif choice == 'h':
         vid = 'hard.MP4'
-        n_boxes = 1
+        break
+    else:
+        print('INVALID INPUT. Try again.')
+        
+# Choose number of objects to track
+while True:
+    choice = input("Would you like to track 1, 2, or 3 objects? ")
+    if choice == '1' or choice == '2' or choice == '3':
+        n_box = int(choice)
         break
     else:
         print('INVALID INPUT. Try again.')
@@ -30,5 +36,5 @@ while True:
 rawVideo = cv2.VideoCapture(vid)
 
 # Create Tracking Video
-trackedVideo = objectTracking(rawVideo,n_boxes,max_pts=10,sigma=1,window_size=25)
+trackedVideo = objectTracking(rawVideo,n_box,max_pts=20,sigma=1,window_size=25)
 trackedVideo.release()
