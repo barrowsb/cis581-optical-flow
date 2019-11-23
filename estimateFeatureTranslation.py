9ecfab122f792d408601a8551988b951bc5dbff3
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-23 22:42:31 +0000

    Update estimateFeatureTranslation.py

--- estimateFeatureTranslation.py
+++ estimateFeatureTranslation.py
@@ -24,22 +24,49 @@ def estimateFeatureTranslation(startX,startY,Ix,Iy,img1,img2,window_size):
 
     # Temporal Gradient
     It = img2 - img1
-    
-    # Use these???
-    #x_grid, y_grid = np.meshgrid(n_cols, n_rows)
-    #f = interp2d(x_grid, y_grid)
-    
+        
     # Center of Window is an individual feature coordinate
     x_center = startX
     y_center = startY
     
+    # Initialize Sums
     sum_Ix_Ix = 0 
     sum_Ix_Iy = 0
     sum_Iy_Iy = 0
     sum_Ix_It = 0
     sum_Iy_It = 0
-    TL_x = int(x_center - math.floor(size_window/2))
-    TL_y = int(y_center - math.floor(size_window/2))
+    
+    '''
+    TL_x = int(x_center - math.floor(size_window/2))  # Top Left X Coordinate of Window
+    TL_y = int(y_center - math.floor(size_window/2))  # Top Left Y Coordinate of Window
+    '''
+    
+    
+    # Meshgrid
+    x_grid, y_grid = np.meshgrid(np.arange(window_size), np.arange(window_size))
+    TL_x = int(x_center - math.floor(size_window/2))  # Top Left X Coordinate of Window
+    TL_y = int(y_center - math.floor(size_window/2))  # Top Left Y Coordinate of Window
+    x_grid_window = x_grid + TL_x
+    y_grid_window = y_grid + TL_y
+    x_flat = x_grid_window.ravel()
+    y_flat = y_grid_window.ravel()
+    
+    # Interpolation
+    img1_interp = interp2(img1, x_flat, y_flat)
+    img2_interp = interp2(img2, x_flat, y_flat)
+    Ix_interp = interp2(Ix, x_flat, y_flat)
+    Ix_interp.reshape(-1,1)
+    Iy_interp = interp2(Iy, x_flat, y_flat)
+    Iy_interp.reshape(-1,1)
+    It = img2_interp - img1_interp  
+        
+    Ix_Iy = np.zeros((size_window*size_window,2))
+    Ix_Iy[:,0] = Ix_interp
+    Ix_Iy[:,1] = Iy_interp
+    gradientMatrix = np.dot(np.transpose(Ix_Iy), Ix_Iy)    
+    temporalMatrix = -np.dot(np.transpose(Ix_Iy), It)
+    
+    '''
     
     # Loop through window around feature
     for r in range(size_window):
@@ -56,14 +83,16 @@ def estimateFeatureTranslation(startX,startY,Ix,Iy,img1,img2,window_size):
             
     # b Matrix in KLT Linear System Equation
     temporalMatrix = -np.array([[sum_Ix_It],[sum_Iy_It]])
-        
+    
+    '''
+    
     # Solving Linear System Equation    
-    A = np.squeeze(np.linalg.inv(gradientMatrix))
+    A = np.squeeze(np.linalg.pinv(gradientMatrix))
     b = np.squeeze(temporalMatrix)
     flow = np.dot(A,b)
     
-    newX = startX + math.ceil(flow[0])
-    newY = startY + math.ceil(flow[1])
+    newX = startX + (flow[0])
+    newY = startY + (flow[1])
     
     #print(flow)
     
