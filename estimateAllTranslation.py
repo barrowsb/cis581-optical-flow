c255efba1bb6a1df2ac89632318247e9c5f64ee7
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-24 22:00:31 +0000

    Update estimateAllTranslation.py

--- estimateAllTranslation.py
+++ estimateAllTranslation.py
@@ -2,6 +2,8 @@
 # (INPUT) startYs: NxF matrix representing the starting Y coordinates of all the features in the first frame for all the bounding boxes
 # (INPUT) img1: HxWx3 matrix representing the first image frame
 # (INPUT) img2: HxWx3 matrix representing the second image frame
+# (INPUT) sigma: An integer that applies to the gaussian blur
+# (INPUT) window_size: An integer representing the side length of a square that defines the feature window
 # (OUTPUT) newXs: NxF matrix representing the new X coordinates of all the features in all the bounding boxes
 # (OUTPUT) newYs: NxF matrix representing the new Y coordinates of all the features in all the bounding boxes
 
